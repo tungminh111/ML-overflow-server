6b1a547b1e6e7d98127adea2f4cc821756c6d4ac
longvb <vuongbaolong48@gmail.com>
2021-09-23 01:37:19 +0700

    add authentication

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,9 @@
 const { commonModule } = require("./common-module");
 const { userModule } = require("./user");
+const { authenticationModule } = require("./authentication");
 
 module.exports = {
     commonModule,
     userModule,
+    authenticationModule,
 };
